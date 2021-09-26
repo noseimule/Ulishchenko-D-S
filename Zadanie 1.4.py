de515a9a70a0e36595f0f47d6970c5e2d9a6a01a
noseimule <90968282+noseimule@users.noreply.github.com>
2021-09-26 10:39:15 +0300

    Add files via upload

---
+++ Zadanie 1.4.py
@@ -0,0 +1,7 @@
+# _*_ coding: utf-8 _*_
+print ('Введите количество секунд')
+seconds=int(input())
+days = seconds / 86400
+hours = seconds / 3600
+minutes = seconds / 60
+print (days ,'дней:', hours ,'часов:', minutes ,'минут:', seconds ,'секунд')
