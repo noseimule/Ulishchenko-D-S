9d7159a389a8b437eecc36b40b8522a31cbb38d4
noseimule <90968282+noseimule@users.noreply.github.com>
2021-09-30 21:51:31 +0300

    Delete Zadanie 2.1.py

--- Zadanie 2.1.py
+++
@@ -1,9 +0,0 @@
-# _*_ coding: utf-8 _*_
-print('Введите первое число')
-x = int(input())
-print('Введите второе число')
-y = int(input())
-print('Введите третье число')
-z = int(input())
-n = x+y+z
-print(n)
